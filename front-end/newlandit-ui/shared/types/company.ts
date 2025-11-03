080b61c5ed5293b47214091ee03333ff510589bb
D Newland <kingxD107@hotmail.com>
2025-11-03 20:24:52 +0100

    add ContactInfo interface and initial contact data

---
+++ front-end/newlandit-ui/shared/types/company.ts
@@ -0,0 +1,12 @@
+export interface ContactInfo {
+  phone: string
+  phoneHref: string
+  email: string
+  emailHref: string
+  website: string
+  websiteDisplay?: string
+  kvk: string
+  btw: string
+  startYear: number
+}
+
---
+++ front-end/newlandit-ui/shared/utils/contact.ts
@@ -0,0 +1,14 @@
+import type { ContactInfo } from '../types/company'
+
+export const CONTACT: ContactInfo = {
+  phone: '+31 648 364 450',
+  phoneHref: 'tel:+31648364450',
+  email: 'info@newlandit-solutions.com',
+  emailHref: 'mailto:info@newlandit-solutions.com',
+  website: 'https://www.newlandit-solutions.com',
+  websiteDisplay: 'www.newlandit-solutions.com',
+  kvk: '83541934',
+  btw: 'NL003835137B29',
+  startYear: 2021,
+}
+
