65a55a0aeddeac8b76363b3a45c10fd315a89176
D Newland <kingxD107@hotmail.com>
2025-11-04 19:28:23 +0100

    :art: enhance HeroSection component: add layout structure and styling elements

--- front-end/newlandit-ui/app/components/HeroSection.vue
+++ front-end/newlandit-ui/app/components/HeroSection.vue
@@ -1,5 +1,21 @@
 <template>
+<section>
+<!--  <div class="bg-violet-200 h-10 w-full border-2 border-violet-600 rounded-lg my-4 p-6 flex justify-center items-center">
+&lt;!&ndash;    <h1 class="text-center font-mono font-extrabold">HeroSection</h1>&ndash;&gt;
+    <h1 class="text-center font-mono font-extrabold text-[30px]"> JSMastery.pro</h1>
+  </div>-->
+  <div class="flex flex-col items-center justify-center space-y-2 mt-2">
+    <div class="h-16 w-16 rounded-full bg-violet-500"></div>
+    <div class="h-16 w-16 rounded-full bg-yellow-500"></div>
+    <div class="h-16 w-16 rounded-full bg-red-500"></div>
+  </div>
 
+  <div class="grid grid-cols-3 gap-2 mt-2 mx-2">
+    <div class="h-16  rounded-full bg-blue-500"></div>
+    <div class="h-16  rounded-full bg-green-500"></div>
+    <div class="h-16  rounded-full bg-orange-500"></div>
+  </div>
+</section>
 </template>
 
 <script setup lang="ts">
@@ -7,5 +23,7 @@
 </script>
 
 <style scoped>
-
+.test{
+  background: darkturquoise;
+}
 </style>
